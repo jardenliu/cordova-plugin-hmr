2ab6d9a954b871e670187b3a8a2f874ffd2cb08a
jardenliu <15915839707@163.com>
2019-04-25 23:28:01 +0800

    feat: created inject plugin

---
+++ src/osx/CDVRemoteInject.h
@@ -0,0 +1,24 @@
+#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
+
+@interface CDVRemoteInject : CDVPlugin
+{
+    // CFRunLoopSourceRef runLoopSource;
+    // float level;
+    // bool isPlugged;
+    // NSString *callbackId;
+}
+
+// @property(nonatomic) CFRunLoopSourceRef runLoopSource;
+// @property(nonatomic) float level;
+// @property(nonatomic) bool isPlugged;
+// @property(strong) NSString *callbackId;
+
+// - (void)start:(CDVInvokedUrlCommand *)command;
+// - (void)stop:(CDVInvokedUrlCommand *)command;
+// - (NSDictionary *)getBatteryStatus;
+// - (void)dealloc;
+
+// void handlePowerSourceChange(CDVOSXBattery *context);
+
+@end
